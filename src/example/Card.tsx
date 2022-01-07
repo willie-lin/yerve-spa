c56182c43fc274c8f4cae447ae146c3108e77511
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-01-07 14:55:51 +0800

    aa

---
+++ src/example/Card.tsx
@@ -0,0 +1,18 @@
+import React, {FunctionComponent} from "react";
+
+
+type CardPrpos = {
+    title: string,
+    paragraph: string
+}
+
+// export const Card = ({title, paragraph} : CardPrpos) => <aside>
+//     <h2>{ title }</h2>
+//     <p>
+//         { paragraph }
+//     </p>
+// </aside>
+//
+// const el = <Card title="WelCome!" paragraph="To this example" />
+
+// export const Card: FunctionComponent
